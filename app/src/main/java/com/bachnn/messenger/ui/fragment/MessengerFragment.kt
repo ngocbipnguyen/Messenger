2c5ac3e995352b42942285c1ac493aa5f47344be
bachnn <ngocbipnguyen@gmail.com>
2025-02-04 17:10:41 +0700

    open_camera

--- app/src/main/java/com/bachnn/messenger/ui/fragment/MessengerFragment.kt
+++ app/src/main/java/com/bachnn/messenger/ui/fragment/MessengerFragment.kt
@@ -1,12 +1,18 @@
 package com.bachnn.messenger.ui.fragment
 
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bachnn.messenger.R
 import com.bachnn.messenger.base.BaseFragment
 import com.bachnn.messenger.constants.Constants
 import com.bachnn.messenger.data.model.Message
@@ -14,7 +20,12 @@ import com.bachnn.messenger.data.model.User
 import com.bachnn.messenger.databinding.MessengerFragmentBinding
 import com.bachnn.messenger.ui.adapter.MessageAdapter
 import com.bachnn.messenger.ui.viewModel.MessengerViewModel
+import com.google.firebase.storage.FirebaseStorage
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.io.File
+import java.text.SimpleDateFormat
 import java.util.Date
 
 @AndroidEntryPoint
@@ -27,6 +38,20 @@ class MessengerFragment : BaseFragment<MessengerViewModel, MessengerFragmentBind
     var isSendingVisibility: Boolean = false
 
     var messages = ArrayList<Message>()
+
+    private lateinit var uriImage: Uri
+    private lateinit var pathFile: File
+    private lateinit var dateCamera: Date
+
+    val openCameraIntent = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+        if (it) {
+            // save image in cloud and delete file image save in cache.
+            lifecycleScope.launch {
+                uploadImages(uriImage)
+            }
+        }
+    }
+
 
     override fun createViewModel(): MessengerViewModel {
         return ViewModelProvider(this)[MessengerViewModel::class]
@@ -103,6 +128,10 @@ class MessengerFragment : BaseFragment<MessengerViewModel, MessengerFragmentBind
             }
         }
 
+        binding.cameraIcon.setOnClickListener {
+            //todo open camera.
+            openCamera()
+        }
 
     }
 
@@ -119,6 +148,31 @@ class MessengerFragment : BaseFragment<MessengerViewModel, MessengerFragmentBind
             rightHolder.sendingText.visibility = visibility
             isSendingVisibility = visibility == View.VISIBLE
         }
+    }
+
+    private fun openCamera() {
+        dateCamera = Date()
+        val formatDate = SimpleDateFormat("yyyy-MM-dd:HH:mm:ss")
+        val strDate = formatDate.format(dateCamera)
+
+        val pathFile = File(
+            requireContext().cacheDir,
+            "${requireContext().getString(R.string.app_name)}_$strDate.jpg"
+        )
+        this.pathFile = pathFile
+        uriImage = FileProvider.getUriForFile(requireContext(), "com.bachnn.messenger", pathFile)
+        openCameraIntent.launch(uriImage)
+    }
+
+    private suspend fun uploadImages(uriImage: Uri) {
+        val timestamps = dateCamera.time.toString()
+        val folderMedia = "${viewModel.group}/$timestamps"
+        FirebaseStorage.getInstance().reference.child(folderMedia).putFile(uriImage).await()
+        val url = FirebaseStorage.getInstance().reference.child(folderMedia).downloadUrl.await()
+
+        Log.e("uploadImages", "url : $url")
+        viewModel.sendMessage(userTo.uid, uriImage.toString(), Constants.TYPE_IMAGE, timestamps, userTo.token)
+
     }
 
 }
